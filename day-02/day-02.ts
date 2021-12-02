c7a6bba256d50e69236392bf7ad30e5d7869e741
Eduardo Preciado <hi@ejpreciado.com>
2021-12-02 00:33:55 -0500

    Solve part 2 of day 2

--- day-02/day-02.ts
+++ day-02/day-02.ts
@@ -12,5 +12,20 @@ const calculateDistance = (data: string[]): Record<string, number> => {
   return { horizontal, depth };
 };
 
+const calculateAimDistance = (data: string[]): Record<string, number> => {
+  let horizontal = 0, depth = 0, aim = 0;
+  data.forEach((row) => {
+    const [direction, distance] = row.split(' ');
+    if (direction == 'forward') {
+      horizontal += +distance;
+      depth += (aim * +distance);
+    }
+    else {
+      aim += (direction == 'up' ? -1 : 1) * +distance;
+    }
+  });
+  return { horizontal, depth, aim };
+};
 const data = fs.readFileSync('./input.txt', 'utf-8').split('\n');
 console.log(calculateDistance(data));
+console.log(calculateAimDistance(data));
